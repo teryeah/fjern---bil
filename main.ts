6951afb32048b7a3ffbcecfc8a83ab7adc6621fd
TerYeah <teryeah@gmail.com>
2020-11-09 18:14:15 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,30 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
-        bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+        bitbot.rotatems(BBRobotDirection.Left, 40, 200)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (receivedNumber == 3) {
-        bitbot.rotatems(BBRobotDirection.Right, 60, 400)
+        bitbot.rotatems(BBRobotDirection.Right, 40, 200)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     } else if (receivedNumber == 2) {
-        bitbot.goms(BBDirection.Forward, 60, 400)
-    } else {
+        bitbot.go(BBDirection.Forward, 60)
+        basic.showIcon(IconNames.Heart)
+    } else if (receivedNumber == 4) {
         bitbot.stop(BBStopMode.Brake)
+        basic.showIcon(IconNames.Sad)
+    } else {
+    	
     }
 })
 input.onButtonPressed(Button.A, function () {
@@ -15,10 +33,21 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(3)
 })
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(4)
+})
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(1)
 })
 radio.setGroup(29)
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    if (bitbot.sonar(BBPingUnit.Centimeters) < 20) {
+        bitbot.ledRainbow()
+        bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    } else {
+        bitbot.ledClear()
+    }
 })
